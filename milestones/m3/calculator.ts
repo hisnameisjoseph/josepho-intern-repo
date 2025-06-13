51634fe56e7116a97bff0ac5ffcd2fa80cfb9bcd
hisnameisjoseph <57517711+hisnameisjoseph@users.noreply.github.com>
2025-06-13 15:29:38 +1000

    feat: add divide function

--- milestones/m3/calculator.ts
+++ milestones/m3/calculator.ts
@@ -11,3 +11,10 @@ export function subtract(a: number, b: number): number {
 export function multiply(a: number, b: number): number {
   return a * b;
 }
+
+export function divide(a: number, b: number): number {
+  if (b === 0) {
+    throw new Error("Division by zero");
+  }
+  return a / b;
+}
