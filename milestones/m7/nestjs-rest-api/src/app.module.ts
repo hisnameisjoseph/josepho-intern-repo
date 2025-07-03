aaf91760f0b3d5810be0eda4fc5b5eab13fd3248
hisnameisjoseph <josephforanu@gmail.com>
2025-07-03 13:28:12 +1000

    chore: remove user folder in nestjs-rest-api
    Will be using [32mCREATE[39m src/user/user.controller.spec.ts (556 bytes)
    [32mCREATE[39m src/user/user.controller.ts (883 bytes)
    [32mCREATE[39m src/user/user.module.ts (241 bytes)
    [32mCREATE[39m src/user/user.service.spec.ts (446 bytes)
    [32mCREATE[39m src/user/user.service.ts (607 bytes)
    [32mCREATE[39m src/user/dto/create-user.dto.ts (30 bytes)
    [32mCREATE[39m src/user/dto/update-user.dto.ts (169 bytes)
    [32mCREATE[39m src/user/entities/user.entity.ts (21 bytes)
    [36mUPDATE[39m package.json (621 bytes) CLI to generate the directory for CRUD task in issue #35

--- milestones/m7/nestjs-rest-api/src/app.module.ts
+++ milestones/m7/nestjs-rest-api/src/app.module.ts
@@ -4,8 +4,26 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
 
+// Added TypeOrmModule, ConfigModule for issue #35
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+
 @Module({
-  imports: [AuthModule, PostsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // This is good for dev only, disable in prod
+    }),
+    AuthModule, PostsModule, UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
